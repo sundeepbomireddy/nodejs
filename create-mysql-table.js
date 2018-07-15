de703eece05c4c55f1b1b6f196ec73414050ae18
sundeepbomireddy <sundeepbomireddy@gmail.com>
2018-07-15 15:17:38 +0530

    create mysql table in nodejs

---
+++ create-mysql-table.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+var conn = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'',
+	database:'node'
+});
+
+var sql_query = 'create table customers(`id` int(11) AUTO_INCREMENT not null primary key,`name` varchar(50) not null,`age` int(3) null )';
+conn.connect(function(err){
+	if(err)
+		throw err;
+	conn.query(sql_query, function(err, result){
+		if(err)
+			throw err;
+		console.log('Table created successfully');
+    });
+})
